4fdfb0a4b5aa385b551f4c9bccf5185ac5973c5d
sarvagya-ui <sarvagya.pandey@outlook.com>
2021-08-06 17:53:12 +0530

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { FilterComponent } from './components/shopping-cart/filter/filter.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { CartItemComponent } from './components/shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './components/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FilterComponent,
+    CartComponent,
+    ProductListComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
